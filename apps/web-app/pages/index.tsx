f16cf6fdc7ec20e684430bc4853cee4736c7d4aa
danilowhk <danilowhk@gmail.com>
2023-04-23 19:36:04 -0300

    add clear cache based on version

--- apps/web-app/pages/index.tsx
+++ apps/web-app/pages/index.tsx
@@ -1,29 +1,50 @@
 // pages/index.tsx
-import { GetServerSideProps } from "next"
-import { EventsDTO } from "../types"
-import HomeTemplate from "../templates/Home"
+import { GetServerSideProps } from "next";
+import { useEffect } from "react";
+import { EventsDTO } from "../types";
+import HomeTemplate from "../templates/Home";
 
 type Props = {
-    events: EventsDTO[]
-}
+  events: EventsDTO[];
+};
 
-const Home = ({ events }: Props) => <HomeTemplate events={events} />
+const Home = ({ events }: Props) => {
+  const currentVersion = "1.2.0";
+  const storageVersionKey = "myAppVersion";
 
-export default Home
+  function clearAllStorage() {
+    // Clear LocalStorage
+    localStorage.clear();
+    // Clear SessionStorage
+    sessionStorage.clear();
+    // Clear IndexedDB
+    window.indexedDB.deleteDatabase("yourDatabaseName");
+  }
+
+  function checkAndUpdateVersion() {
+    const storedVersion = localStorage.getItem(storageVersionKey);
+    if (storedVersion !== currentVersion) {
+      clearAllStorage();
+      localStorage.setItem(storageVersionKey, currentVersion);
+    }
+  }
+
+  useEffect(() => {
+    checkAndUpdateVersion();
+  }, []);
+
+  return <HomeTemplate events={events} />;
+};
+
+export default Home;
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-    try {
-        const url = process.env.URL_TO_FETCH
+  try {
+    const url = process.env.URL_TO_FETCH;
 
-        const eventsResponse = await fetch(`${url}/api/fetchEvents`)
-        const events = await eventsResponse.json()
-        return {
-            props: { events }
-        }
-    } catch (error) {
-        res.statusCode = 404
-        return {
-            props: {}
-        }
-    }
-}
+    const eventsResponse = await fetch(`${url}/api/fetchEvents`);
+    const events = await eventsResponse.json();
+    return {
+      props: { events },
+    };
+  }
