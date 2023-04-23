d451e72eef4ee7095bd96f19f7c36633c09e6af9
danilowhk <danilowhk@gmail.com>
2023-04-23 13:11:27 -0300

    change api endpoints

--- apps/web-app/components/SigInModal/index.jsx
+++ apps/web-app/components/SigInModal/index.jsx
@@ -1,31 +1,38 @@
-import { createClient } from '@supabase/supabase-js'
+// components/SignInModal/index.js
 import SignInModalView from "./view.jsx";
 
-const supabaseUrl = "https://polcxtixgqxfuvrqgthn.supabase.co"
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-
 const SignInModal = ({ isOpen, closeModal, checkSession }) => {
-
   async function handleSignIn(event) {
     event?.preventDefault();
-    const email = event.target[0].value
-    const password = event.target[1].value
-    try {
-      const result = await supabase.auth.signInWithPassword({
-        email,
-        password,
-      });
-      closeModal()
-      checkSession()
-    } catch (error) {
-      closeModal()
-      alert("Not a valid login")
-    }
+    const email = event.target[0].value;
+    const password = event.target[1].value;
 
+    try {
+      const response = await fetch("/api/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        closeModal();
+        checkSession();
+      } else {
+        throw new Error("Invalid login");
+      }
+    } catch (error) {
+      closeModal();
+      alert("Not a valid login");
+    }
   }
 
-  return <SignInModalView isOpen={isOpen} closeModal={closeModal} handleSignIn={handleSignIn} />;
+  return (
+    <SignInModalView
+      isOpen={isOpen}
+      closeModal={closeModal}
+      handleSignIn={handleSignIn}
+    />
+  );
 };
 
 export default SignInModal;
--- apps/web-app/hooks/useSupabaseSession.js
+++ apps/web-app/hooks/useSupabaseSession.js
@@ -1,32 +1,38 @@
 import { useState, useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-//TODO: Create api
-const supabaseUrl = "https://polcxtixgqxfuvrqgthn.supabase.co";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
 // eslint-disable-next-line import/prefer-default-export
 export const useSession = () => {
   const [sessionToken, setSessionToken] = useState(null);
 
   useEffect(() => {
     (async () => {
-      const session = await supabase.auth.getSession();
-      console.log(session)
-      setSessionToken(session);
-      console.log(sessionToken);
-      const { data: authListener } = supabase.auth.onAuthStateChange(
-        // eslint-disable-next-line @typescript-eslint/no-shadow
-        (event, session) => {
+      try {
+        const response = await fetch("/api/session");
+        if (response.ok) {
+          const session = await response.json();
+          console.log(session);
           setSessionToken(session);
+          console.log(sessionToken);
+        } else {
+          throw new Error("Unable to get session");
         }
-      );
+      } catch (error) {
+        console.error(error);
+      }
+
+      // Handle auth state change events
+      // TODO: Move this logic to the backend if possible
+      const authListener = () => {
+        // Custom logic to handle authentication events
+        // For example, you can use WebSockets to listen for auth state changes on the server
+      };
 
       return () => {
-        authListener.unsubscribe();
+        // Cleanup the auth listener
+        // For example, close the WebSocket connection
       };
-    })()
+    })();
   }, [sessionToken]);
 
   return sessionToken;
