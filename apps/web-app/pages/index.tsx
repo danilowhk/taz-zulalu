c0f8cd9b8d0292a180142c160c46ad5a896997c7
samueloh99 <samuel_oh@hotmail.com>
2023-04-24 03:47:16 +0200

    Changes sam

--- apps/web-app/pages/index.tsx
+++ apps/web-app/pages/index.tsx
@@ -1,92 +1,38 @@
 // pages/index.tsx
-import Dexie from 'dexie';
-import { GetServerSideProps } from "next";
-import { useEffect } from "react";
-import { EventsDTO } from "../types";
-import HomeTemplate from "../templates/Home";
-
+import { GetServerSideProps } from "next"
+import axios from "axios"
+import { EventsDTO } from "../types"
+import HomeTemplate from "../templates/Home"
 
 type Props = {
-  events: EventsDTO[];
-};
+    events: EventsDTO[]
+}
 
-const Home = ({ events }: Props) => {
-  const currentVersion = "1.2.0";
-  const storageVersionKey = "myAppVersion";
+const Home = ({ events }: Props) => <HomeTemplate events={events} />
 
-  async function deleteAllCacheStorage() {
-    const cacheNames = await caches.keys();
-    await Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
-  }
-
-  async function unregisterServiceWorkers() {
-    const registrations = await navigator.serviceWorker.getRegistrations();
-    await Promise.all(registrations.map((registration) => registration.unregister()));
-  }
-
-  async function deleteAllIndexedDB() {
-    try {
-      const dbNames = await Dexie.getDatabaseNames();
-      for (const dbName of dbNames) {
-        await Dexie.delete(dbName);
-      }
-    } catch (error) {
-      console.error('Error deleting IndexedDB databases:', error);
-    }
-  }
-
-  function deleteAllCookies() {
-    document.cookie.split(';').forEach((c) => {
-      document.cookie = c.replace(/=.*/, `=;expires=${  new Date().toUTCString()  };path=/`);
-    });
-  }
-
-  function clearAllStorage() {
-    // Clear LocalStorage
-    localStorage.clear();
-    // Clear SessionStorage
-    sessionStorage.clear();
-    // Clear all IndexedDB databases
-    deleteAllIndexedDB();
-    // Delete all cache storage
-    deleteAllCacheStorage();
-    // Unregister all service workers
-    unregisterServiceWorkers();
-
-    deleteAllCookies();
-  }
-  
-
-  function checkAndUpdateVersion() {
-    const storedVersion = localStorage.getItem(storageVersionKey);
-    if (storedVersion !== currentVersion) {
-      clearAllStorage();
-      localStorage.setItem(storageVersionKey, currentVersion);
-    }
-  }
-  
-  useEffect(() => {
-    checkAndUpdateVersion();
-  }, []);
-
-  return <HomeTemplate events={events} />;
-};
-
-export default Home;
+export default Home
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  try {
-    const url = process.env.URL_TO_FETCH;
+    try {
+        const url = process.env.URL_TO_FETCH
 
-    const eventsResponse = await fetch(`${url}/api/fetchEvents`);
-    const events = await eventsResponse.json();
-    return {
-      props: { events },
-    };
-  } catch (error) {
-    res.statusCode = 404;
-    return {
-      props: {},
-    };
-  }
-};
+        const eventsResponse = await axios.post(
+            `${url}/api/fetchEvents`,
+            {},
+            {
+                headers: {
+                    "x-api-key": process.env.KEY_TO_API as string // Pass cookies from the incoming request
+                }
+            }
+        )
+        const events = await eventsResponse.data
+        return {
+            props: { events }
+        }
+    } catch (error) {
+        res.statusCode = 404
+        return {
+            props: {}
+        }
+    }
+}
