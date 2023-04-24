b2aefcc5ae544bd02c67139ee231e7677e9e1807
samueloh99 <samuel_oh@hotmail.com>
2023-04-24 03:50:13 +0200

    Changes

--- apps/web-app/context/UserPassportContext.tsx
+++ apps/web-app/context/UserPassportContext.tsx
@@ -83,7 +83,7 @@ export function UserPassportContextProvider({ children }: UserPassportProviderPr
         try {
             await axios({
                 method: "post",
-                url: "https://8a25-62-4-33-200.ngrok-free.app/api/passport-user-login/",
+                url: "https://zuzalu.city/api/passport-user-login/",
                 data: { participant1, signatureProofProps },
                 headers: {
                     "Content-Type": "application/json",
